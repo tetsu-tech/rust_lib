2ac044b3c3652ff599c53682d45be87323a6a23e
tetsu5555 <tetete1118@gmail.com>
2022-02-13 19:52:32 +0900

    add packages

---
+++ src/english.rs
@@ -0,0 +1,4 @@
+// hello_world::english::hello の定義
+pub fn hello() -> String {
+    "Hello!".to_string()
+}
---
+++ src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    assert_eq!(hello_world::english::hello(), "Hello!".to_string());
+    println!("{}", hello_world::english::hello());
+}
